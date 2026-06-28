2cc99b364b88f70ded3f68f1f1b7f7f8aa71753e
Lomakina A.S. <amaranthideal@tiffincrane.com>
2026-06-28 23:27:34 +0700

    Lesson 06K

---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_shop():
+    driver = webdriver.Firefox()
+    driver.get("https://www.saucedemo.com/")
+
+    wait = WebDriverWait(driver, 10)
+
+    wait.until(
+        EC.visibility_of_element_located((By.ID, "user-name"))
+    ).send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    wait.until(
+        EC.element_to_be_clickable(
+            (By.ID, "add-to-cart-sauce-labs-backpack")
+        )
+    ).click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, "checkout"))
+    ).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.ID, "first-name"))
+    ).send_keys("Алеся")
+    driver.find_element(By.ID, "last-name").send_keys("Ломакина")
+    driver.find_element(By.ID, "postal-code").send_keys("123456")
+    driver.find_element(By.ID, "continue").click()
+
+    total = wait.until(
+        EC.visibility_of_element_located(
+            (By.CLASS_NAME, "summary_total_label")
+        )
+    ).text
+
+    driver.quit()
+
+    assert total == "Total: $58.29"
